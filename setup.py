16fea397ba43725e25c33f765bdd2766f164ecb1
Chris Perivolaropoulos <darksaga2006@gmail.com>
2012-08-24 14:02:37 +0300

    Version update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "Pyrametros",
-    version = "0.9.3",
+    version = "0.9.5",
     author = "Chris Perivolaropoulos",
     author_email = "darksaga2006@gmail.com",
     description = ("A way to use ascii tables to automatically generate code."),
